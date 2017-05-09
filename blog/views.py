cf7f6237819f3d0e084800c3dd74c904781b5e8d
Sac08 <sachinbagalakoti08@gmail.com>
2017-05-09 07:55:40 +0530

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-view = models.ManyToManyField('view.View',through='ItemView',blank=True)
+#view = models.ManyToManyField('view.View',through='ItemView',blank=True)
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
