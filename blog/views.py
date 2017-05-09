aca78011faaec0773f549554e586e16eb35c48db
Sac08 <sachinbagalakoti08@gmail.com>
2017-05-09 22:16:43 +0530

    CSS part added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-#view = models.ManyToManyField('view.View',through='ItemView',blank=True)
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
+
+
